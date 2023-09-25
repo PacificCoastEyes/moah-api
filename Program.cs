e64b7a2814a9bc698e52367e8b0b343ad66211a8
Amitai Zand <amitai17@gmail.com>
2023-09-25 21:59:15 +0300

    Added CORS code back into app to allow for local testing and debugging. App Service CORS settings override these for production

--- Program.cs
+++ Program.cs
@@ -35,6 +35,15 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://brave-water-053e78e03.3.azurestaticapps.net/", "http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -44,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseAuthentication();
 
